e76e80890dbe3d67c38e0305308b5b95ae38f2fd
yeji Sung <dpwl0332@naver.com>
2024-02-08 14:38:23 +0900

    action value, creator 작성

--- src/redux/modules/letters.js
+++ src/redux/modules/letters.js
@@ -1,3 +1,25 @@
-const letters = () => {};
+import { fakeData } from "shared/DummyData";
+
+//팬레터 추가
+const ADD_LETTER = "letters/ADD_LETTER";
+//팬레터 삭제
+const DELETE_LETTER = "letters/DELETE_LETTER";
+//팬레터 수정
+const EDIT_LETTER = "letters/EDIT_LETTER";
+
+//action creator
+export const addLetter = (payload) => {
+  return { type: ADD_LETTER, payload };
+};
+export const deleteLetter = (payload) => {
+  return { type: DELETE_LETTER, payload };
+};
+export const editLetter = (payload) => {
+  return { type: EDIT_LETTER, payload };
+};
+
+const initialState = fakeData;
+
+const letters = (state = initialState, action) => {};
 
 export default letters;
