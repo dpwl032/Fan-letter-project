c007bfdad4e5fb6f1bbcc7a8c87027db8e10b738
yeji Sung <dpwl0332@naver.com>
2024-02-19 12:21:49 +0900

    youtube 영상 추가

--- src/components/MusicVideo.jsx
+++ src/components/MusicVideo.jsx
@@ -6,14 +6,30 @@ function MusicVideo() {
   return (
     <>
       <YotubVideo>
-        <iframe
+        {/* <iframe
           id="ytplayer"
           type="text/html"
           width="720"
           height="405"
-          src="https://www.youtube.com/embed/M7lc1UVf-VE"
+          src="https://youtu.be/xjBY3MLdvVQ?si=IlL0vQuFQYllKtIN"
           frameBorder="0"
           allowFullScreen
+        /> */}
+        <YouTube
+          videoId="xjBY3MLdvVQ" //동영상 주소
+          opts={{
+            width: "100%",
+            height: "500px",
+            playerVars: {
+              autoplay: 1, //자동 재생 여부
+              modestbranding: 1, //컨트롤 바에 유튜브 로고 표시 여부
+              loop: 1, //반복 재생
+              playlist: "xjBY3MLdvVQ", //반복 재생으로 재생할 플레이 리스트
+            },
+          }}
+          onReady={(e) => {
+            e.target.mute(); //소리 끔
+          }}
         />
       </YotubVideo>
     </>
